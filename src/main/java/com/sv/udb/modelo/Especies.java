c9bc132a035fbb5600ac2fab4cc63398a035d91d
DanielWilfredo <DanielWilfredo@DanielWilfredo>
2017-03-11 09:29:52 -0600

    empezando las clases

---
+++ src/main/java/com/sv/udb/modelo/Especies.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sv.udb.modelo;
+
+/**
+ *
+ * @author DanielWilfredo
+ */
+public class Especies {
+    
+    private int codiEspe;
+    private String nombEspe;
+    private String descEspe;
+    private int codiRefeEspe;
+    private String nombRefeEspe;
+     
+    
+    
+
+    public int getCodiEspe() {
+        return codiEspe;
+    }
+
+    public void setCodiEspe(int codiEspe) {
+        this.codiEspe = codiEspe;
+    }
+
+    public String getNombEspe() {
+        return nombEspe;
+    }
+
+    public void setNombEspe(String nombEspe) {
+        this.nombEspe = nombEspe;
+    }
+
+    public String getDescEspe() {
+        return descEspe;
+    }
+
+    public void setDescEspe(String descEspe) {
+        this.descEspe = descEspe;
+    }
+
+    public int getCodiRefeEspe() {
+        return codiRefeEspe;
+    }
+
+    public void setCodiRefeEspe(int codiRefeEspe) {
+        this.codiRefeEspe = codiRefeEspe;
+    }
+
+    public String getNombRefeEspe() {
+        return nombRefeEspe;
+    }
+
+    public void setNombRefeEspe(String nombRefeEspe) {
+        this.nombRefeEspe = nombRefeEspe;
+    }
+    
+    
+    
+    
+    
+    
+}
